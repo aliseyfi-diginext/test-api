09929a22ebd62037956ba68950bf64ab297760f1
aliseyfi-diginext <aliseyfi.diginext@gmail.com>
2022-02-15 21:34:08 +0330

    cors

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,36 +11,31 @@ class TaskController extends Controller
 
     public function index()
     {
-        $response = Task::where('user_id', auth()->id())->latest()->get();
-        return response($response, 200)->withHeaders([
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Headers' => 'Origin, Content-Type',
-            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS'
-        ]);
+        return Task::where('user_id', auth()->id())->latest()->get();
     }
 
     public function store(TaskRequest $request)
     {
         $data = $request->validated();
         $data['user_id'] = auth()->id();
-        $response = Task::create($data);
+        return Task::create($data);
     }
 
     public function show(Task $task)
     {
-        $response = $task;
+        return $task;
     }
 
     public function update(TaskRequest $request, Task $task)
     {
         $data = $request->validated();
         $task->update($data);
-        $response = $task;
+        return $task;
     }
 
     public function destroy(Task $task)
     {
         $task->delete();
-        $response = response(null, 204);
+        return response(null, 204);
     }
 }
