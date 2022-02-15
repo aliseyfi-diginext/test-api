b347a30adbd8a20de2aa5be7153482f1c354a4a7
aliseyfi-diginext <aliseyfi.diginext@gmail.com>
2022-02-15 22:17:22 +0330

    bug fix

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,31 +11,32 @@ class TaskController extends Controller
 
     public function index()
     {
-        return Task::where('user_id', auth()->id())->latest()->get();
+        $response = Task::where('user_id', auth()->id())->latest()->get();
+        return response($response, 200)->json();
     }
 
     public function store(TaskRequest $request)
     {
         $data = $request->validated();
         $data['user_id'] = auth()->id();
-        return Task::create($data);
+        $response = Task::create($data);
     }
 
     public function show(Task $task)
     {
-        return $task;
+        $response = $task;
     }
 
     public function update(TaskRequest $request, Task $task)
     {
         $data = $request->validated();
         $task->update($data);
-        return $task;
+        $response = $task;
     }
 
     public function destroy(Task $task)
     {
         $task->delete();
-        return response(null, 204);
+        $response = response(null, 204);
     }
 }
