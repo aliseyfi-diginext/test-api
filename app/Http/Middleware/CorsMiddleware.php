deb166ec1ce95467ddf79bbddd8f748553ee31c5
aliseyfi-diginext <aliseyfi.diginext@gmail.com>
2022-02-15 21:50:58 +0330

    cors bug fix

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,8 +17,8 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin: *')
-        ->header('Access-Control-Allow-Methods: *')
-        ->header('Access-Control-Allow-Headers: *');
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', '*')
+        ->header('Access-Control-Allow-Headers', '*');
     }
 }
