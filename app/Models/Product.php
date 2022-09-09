7f8cd94768d227ea0419f2c81c64e04af43fee32
aliseyfi-diginext <aliseyfi.diginext@gmail.com>
2022-09-09 22:50:43 +0430

    picture path

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,10 +10,18 @@ class Product extends Model
     use HasFactory;
     protected $guarded = ['id'];
     protected $hidden = ['user_id'];
-    protected $appends = ['payable'];
+    protected $appends = ['payable', 'picture_path'];
 
     public function getPayableAttribute()
     {
         return $this->discount ? ($this->price - ($this->price * ($this->discount/100))) : $this->price;
+    }
+
+    public function getPicturePathAttribute()
+    {
+        if ($path = $this->picture) {
+            $path = str_replace('public/', 'storage/');
+            return asset($path);
+        }
     }
 }
